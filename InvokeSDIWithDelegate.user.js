6df4d390ff75127c885be2e80e7c2a6018d39cbe
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-08-23 15:08:33 -0500

    Add script to invoke SDI using a delegate

---
+++ InvokeSDIWithDelegate.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Invoke SDI with a delegate
+// @version      0.1
+// @description  Invoke SDI with a delegate
+// @author       You
+// @match        http://localhost:4201/
+// @updateURL    https://raw.githubusercontent.com/dustinspecker/tampermonkey-scripts/master/InvokeSDIWithDelegate.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.postMessage(JSON.stringify({ type: 'INIT', data: { initializationData: { authorEmpId: 100, subjectEmpId: [100], delegateEmpId: 5425 } }}), window.location.origin);
+})();
