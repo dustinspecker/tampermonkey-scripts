0090106d8ed2f442f17cc87db08b724153ddb580
Dustin Specker <dustinspecker@dustinspecker.com>
2016-08-24 08:43:32 -0500

    move applicationName to initializationData in delegate script

--- InvokeSDIWithDelegate.user.js
+++ InvokeSDIWithDelegate.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Invoke SDI with a delegate
-// @version      0.2
+// @version      0.3
 // @description  Invoke SDI with a delegate
 // @author       You
 // @match        http://localhost:4201/
@@ -11,5 +11,5 @@
 (function() {
     'use strict';
 
-    window.postMessage(JSON.stringify({ type: 'INIT', data: { applicationName: 'SDI', initializationData: { authorEmpId: 100, subjectEmpId: [100], delegateEmpId: 5425 } }}), window.location.origin);
+    window.postMessage(JSON.stringify({ type: 'INIT', data: { initializationData: { applicationName: 'SDI', authorEmpId: 100, subjectEmpId: [100], delegateEmpId: 5425 } }}), window.location.origin);
 })();
