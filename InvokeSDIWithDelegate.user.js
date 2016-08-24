1887f6cdcf677d9410b45ea2e7119f5bfd5a5171
Dustin Specker <dustinspecker@dustinspecker.com>
2016-08-24 08:37:11 -0500

    add applicationName to delegate script

--- InvokeSDIWithDelegate.user.js
+++ InvokeSDIWithDelegate.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Invoke SDI with a delegate
-// @version      0.1
+// @version      0.2
 // @description  Invoke SDI with a delegate
 // @author       You
 // @match        http://localhost:4201/
@@ -11,5 +11,5 @@
 (function() {
     'use strict';
 
-    window.postMessage(JSON.stringify({ type: 'INIT', data: { initializationData: { authorEmpId: 100, subjectEmpId: [100], delegateEmpId: 5425 } }}), window.location.origin);
+    window.postMessage(JSON.stringify({ type: 'INIT', data: { applicationName: 'SDI', initializationData: { authorEmpId: 100, subjectEmpId: [100], delegateEmpId: 5425 } }}), window.location.origin);
 })();
